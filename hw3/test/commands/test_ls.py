6942d77990259b319194109ceadc1cecc52f3ea1
Makhnev Konstantin <mah.kons@gmail.com>
2020-02-23 00:34:05 +0300

    fix travis

--- hw3/test/commands/test_ls.py
+++ hw3/test/commands/test_ls.py
@@ -5,12 +5,13 @@ from src.commands.ls import Ls
 class LsTestCase(unittest.TestCase):
     def test_without_args(self):
         ls = Ls([])
-        self.assertEqual("docs\nsrc\nREADME.md\nmain.py\ntest", ls.execute(None))
+        self.assertEqual("README.md\ndocs\nmain.py\nsrc\ntest", '\n'.join(sorted(ls.execute(None).split('\n'))))
 
     def test_with_args(self):
         ls = Ls(['test/resources', 'lol'])
-        self.assertEqual("kek\nkeksik\nshort\nlong", ls.execute(None))
+        self.assertEqual("kek\nkeksik\nlong\nshort", '\n'.join(sorted(ls.execute(None).split('\n'))))
 
     def test_unknown_dir(self):
         ls = Ls(['kek', 'lol'])
         self.assertEqual("ls: kek: No such file or directory\n", ls.execute('kek'))
+
